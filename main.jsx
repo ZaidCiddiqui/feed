65d923e26df25c4a715c05bed1d002fc8e102202
Zaid Ahmed <40208065+ZaidCiddiqui@users.noreply.github.com>
2026-05-08 11:00:43 +1000

    Delete main.jsx

--- main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-   import ReactDOM from 'react-dom/client'
-   import WifeyFeed from './WifeyFeed.jsx'
-
-   ReactDOM.createRoot(document.getElementById('root')).render(<WifeyFeed />)
